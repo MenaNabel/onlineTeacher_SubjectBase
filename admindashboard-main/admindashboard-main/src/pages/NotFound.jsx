cda28df4a4930216dab4a9cd20660c5d67c545ee
Hossam Fathi <hosamfathi108@gmail.com>
2023-07-04 21:45:14 +0300

    All Project

---
+++ admindashboard-main/admindashboard-main/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom'
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div className="notfound">
+        <img src="./images/404.svg" alt="" />
+        <h5>الصفحة غير موجوده</h5>
+        <Link className="return-home" to="/home">العودة للرئيسية</Link>
+      </div>  
+    )
+  }
+}
